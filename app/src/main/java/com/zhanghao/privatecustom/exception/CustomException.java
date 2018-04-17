39af113bf60ce0a5a6a5f4f8d1ab155c8d062b43
zhanghao <zhanghao97hao@163.com>
2018-04-17 19:47:48 +0800

    Initial commit

---
+++ app/src/main/java/com/zhanghao/privatecustom/exception/CustomException.java
@@ -0,0 +1,18 @@
+package com.zhanghao.privatecustom.exception;
+
+public class CustomException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public CustomException(){
+        super();
+    }
+    public CustomException(String detailMessage,Throwable throwable){
+        super(detailMessage,throwable);
+    }
+    public CustomException(String detailMessage){
+        super(detailMessage);
+    }
+    public CustomException(Throwable throwable){
+        super(throwable);
+    }
+}
